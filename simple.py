431814952073e86b35654631bc903f8167df3751
SamWhitehead <whitehead.sam@gmail.com>
2013-07-28 20:52:52 +0100

    update 28July

--- simple.py
+++ simple.py
@@ -22,7 +22,23 @@ def simple2():
     plt.setp(line1, color='m', linewidth=3.0)
     plt.show()
 
+def simple3():
+    
+    def f(t):
+        return np.exp(-t) * np.cos(2*np.pi*t)
+
+    t1 = np.arange(0.0, 5.0, 0.1)
+    t2 = np.arange(0.0, 5.0, 0.02)
+
+    plt.figure(1)
+    plt.subplot(211)
+    plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+
+    plt.subplot(212)
+    plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
+
+    plt.show()
 
 
 if __name__ == "__main__":
-    simple2()
+    simple3()
